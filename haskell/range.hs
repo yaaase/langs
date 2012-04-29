5dee4d72bde3c7561a5d654cbb6f074cea6ef5d0
Mark Billie <mbillie1@gmail.com>
2012-04-28 23:49:23 -0500

    changes

--- haskell/range.hs
+++ haskell/range.hs
@@ -1,7 +1,5 @@
 module Main where
 
-  myRange start step = start:(myRange (start + step) step)
-
   lazyFib x y = x:(lazyFib y (x + y))
   fib = lazyFib 1 1
   fibNth x = head (drop (x - 1) (take (x) fib))
