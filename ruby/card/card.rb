b107e0d20488d1ecc52b4148c6e20de7f01f9431
Mark Billie <mbillie1@gmail.com>
2012-08-29 01:25:50 -0400

    s/ /\s/

--- ruby/card/card.rb
+++ ruby/card/card.rb
@@ -4,7 +4,7 @@ g="ekc";o="h y";h="u";"r#{g}a#{o}b#{h}r".reverse
 ->{x="ekc";y="h yb";"r#{x}a#{y}ur".reverse}[]
 
 
-->(g){m=" yb";"r#{g}ah#{m}ur".reverse}["ekc"]
+->(g){m="\syb";"r#{g}ah#{m}ur".reverse}["ekc"]
 
 
 ->(r,a){"r#{r}a#{a}ur".reverse}["ekc","h yb"]
