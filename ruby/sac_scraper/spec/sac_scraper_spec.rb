4a8c9ddd4eca5a6f3e64131955b9271175bbfba0
Mark Billie <mbillie1@gmail.com>
2013-02-28 23:52:43 -0500

    fixing test

--- ruby/sac_scraper/spec/sac_scraper_spec.rb
+++ ruby/sac_scraper/spec/sac_scraper_spec.rb
@@ -16,13 +16,13 @@ describe SteepAndCheapScraper do
   context "remembering the title" do
     it "lets me know what the cool shit is" do
       s.stub(:scrape_title).and_return("Black Diamond Verdict Ski")
-      s.should_receive(:mail!).with("Black Diamond Verdict Ski")
+      s.should_receive(:mail!).with("Black Diamond Verdict Ski http://www.steepandcheap.com/")
       s.scrape!
     end
 
     it "does not spam me if the match is the same as before" do
       s.stub(:scrape_title).and_return("Black Diamond Verdict Ski")
-      s.instance_variable_set(:@previous_title, "Black Diamond Verdict Ski")
+      s.instance_variable_set(:@previous_title, "Black Diamond Verdict Ski http://www.steepandcheap.com/")
       s.should_not_receive(:mail!).with("Black Diamond Verdict Ski")
       s.scrape!
     end
