02691169239b6a4ba7c9969f9b0f3fab512bfec1
Mark Billie <mbillie1@gmail.com>
2013-03-05 17:42:04 -0500

    hoping to get this starting to sort of almost maybe work

---
+++ ruby/rslint/lib/file_parser.rb
@@ -0,0 +1,41 @@
+require_relative './lint'
+
+class FileParser
+  attr_reader :lines
+
+  def initialize(file_path, lint)
+    @lines = []
+    @lint = lint
+
+    file = File.open(file_path, 'r')
+    file.readlines.each_with_index do |line, number|
+      @lines << [line, number + 1]
+    end
+  end
+
+  def violations?
+    @lines.each do |array|
+      @lint.violation?(array[0], array[1])
+    end
+
+    if @lint.errors.any?
+      display_errors
+      true
+    else
+      false
+    end
+  end
+
+  def display_errors
+    puts "You have the following errors:"
+    @lint.errors.each do |hash|
+      error = hash.keys.first
+      line_number = hash.values.first
+      puts "#{@lint.send(error)} on line #{line_number}"
+    end
+  end
+end
+
+if ARGV[0] == 'check' && ARGV[1]
+  FileParser.new(ARGV[1], Lint.new).violations?
+end
---
+++ ruby/rslint/spec/file_parser_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../lib/file_parser'
+
+describe FileParser do
+  context "pass" do
+    before(:each) do
+      @string = "/home/mark/langs/ruby/rslint/txt/sample_1.rb"
+      @f = FileParser.new(@string, Lint.new)
+    end
+
+    it "grabs a file with its line numbers" do
+      @f.lines[0].should == ["class Foo\n", 1]
+    end
+
+    it "checks the file for errors" do
+      @f.violations?.should be_false
+    end
+  end
+
+  context "fail" do
+    before(:each) do
+      @string = "/home/mark/langs/ruby/rslint/txt/sample_fail.rb"
+      @f = FileParser.new(@string, Lint.new)
+    end
+
+    it "knows when something has failed" do
+      puts @f.violations?
+    end
+  end
+end
