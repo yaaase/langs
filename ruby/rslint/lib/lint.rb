6c4fbf35cb0e045c84057aa2ad89e1fd8c9d19da
Mark Billie <mbillie1@gmail.com>
2013-03-06 22:20:30 -0500

    i guess i'll align all the hash rockets

--- ruby/rslint/lib/lint.rb
+++ ruby/rslint/lib/lint.rb
@@ -2,33 +2,33 @@ class Lint
   attr_reader :errors
 
   LineTooLongViolations = {
-    /.{80}+/                     => :line_too_long
+    /.{80}+/                      => :line_too_long
   }
 
   Violations = {
-    /def (self\.)?\w+[!?]? .\w+/ => :missing_parens,
-    /( )+$/                      => :trailing_whitespace,
-    /\band\b/                    => :the_word_and,
-    /\bor\b/                     => :the_word_or,
-    /\bfor\b/                    => :the_word_for,
-    /\bif\b.*\bthen\b\n/         => :multiline_if_then,
-    /\(\s|\s\)/                  => :paren_spacing,
-    /\[\s|\s\]/                  => :bracket_spacing,
-    /[^\s][{}]|{[^\s]/           => :brace_spacing,
-    /,[^ \n]/                    => :comma_spacing
+    /def (self\.)?\w+[!?]? .\w+/  => :missing_parens,
+    /( )+$/                       => :trailing_whitespace,
+    /\band\b/                     => :the_word_and,
+    /\bor\b/                      => :the_word_or,
+    /\bfor\b/                     => :the_word_for,
+    /\bif\b.*\bthen\b\n/          => :multiline_if_then,
+    /\(\s|\s\)/                   => :paren_spacing,
+    /\[\s|\s\]/                   => :bracket_spacing,
+    /[^\s][{}]|{[^\s]/            => :brace_spacing,
+    /,[^ \n]/                     => :comma_spacing
   }
 
   ExceptionViolations = {
-    /rescue\s*(Exception)?$/ => :rescue_class_exception
+    /rescue\s*(Exception)?$/      => :rescue_class_exception
   }
 
   MetaprogrammingViolations = {
-    /\beval\b/          => :used_eval,
-    /\bclass_eval\b/    => :used_class_eval,
-    /\bmodule_eval\b/   => :used_module_eval,
-    /\binstance_eval\b/ => :used_instance_eval,
-    /define_method/     => :used_define_method,
-    /\w+\.send.*".*#\{/ => :dynamic_invocation
+    /\beval\b/                    => :used_eval,
+    /\bclass_eval\b/              => :used_class_eval,
+    /\bmodule_eval\b/             => :used_module_eval,
+    /\binstance_eval\b/           => :used_instance_eval,
+    /define_method/               => :used_define_method,
+    /\w+\.send.*".*#\{/           => :dynamic_invocation
   }
 
   CommentedLine = [
@@ -36,24 +36,24 @@ class Lint
   ]
 
   Messages = {
-    :missing_parens         => "You have omitted parentheses from a method definition with parameters.",
-    :line_too_long          => "Line length of 80 characters or more.",
-    :trailing_whitespace    => "Trailing whitespace.",
-    :used_eval              => "Use of eval.",
-    :used_define_method     => "Use of define_method.",
-    :dynamic_invocation     => "Dynamic invocation of a method.",
-    :rescue_class_exception => "Rescuing class Exception.",
-    :the_word_and           => "Used 'and'; please use && instead.",
-    :the_word_or            => "Used 'or'; please use || instead.",
-    :the_word_for           => "Used 'for'; please use an enumerator, or else explain yourself adequately to the team.",
-    :multiline_if_then      => "Use of 'then' on a multiline if statement.",
-    :used_class_eval        => "Use of class_eval.",
-    :used_module_eval       => "Use of module_eval.",
-    :used_instance_eval     => "Use of instance_eval.",
-    :paren_spacing          => "Space after ( or before ).",
-    :bracket_spacing        => "Space after [ or before ].",
-    :brace_spacing          => "No space around { or before }.",
-    :comma_spacing          => "No space after a comma."
+    :missing_parens               => "You have omitted parentheses from a method definition with parameters.",
+    :line_too_long                => "Line length of 80 characters or more.",
+    :trailing_whitespace          => "Trailing whitespace.",
+    :used_eval                    => "Use of eval.",
+    :used_define_method           => "Use of define_method.",
+    :dynamic_invocation           => "Dynamic invocation of a method.",
+    :rescue_class_exception       => "Rescuing class Exception.",
+    :the_word_and                 => "Used 'and'; please use && instead.",
+    :the_word_or                  => "Used 'or'; please use || instead.",
+    :the_word_for                 => "Used 'for'; please use an enumerator, or else explain yourself adequately to the team.",
+    :multiline_if_then            => "Use of 'then' on a multiline if statement.",
+    :used_class_eval              => "Use of class_eval.",
+    :used_module_eval             => "Use of module_eval.",
+    :used_instance_eval           => "Use of instance_eval.",
+    :paren_spacing                => "Space after ( or before ).",
+    :bracket_spacing              => "Space after [ or before ].",
+    :brace_spacing                => "No space around { or before }.",
+    :comma_spacing                => "No space after a comma."
   }
 
   def initialize
