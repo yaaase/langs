25b1aecb1402b258751eae7f57fb4e8825ef412b
Mark Billie <mbillie1@gmail.com>
2014-03-31 16:14:57 -0600

    rss scraper

---
+++ ruby/rss_scrape/scrape.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+require 'net/smtp'
+require 'uri'
+require 'rss'
+
+URL = "http://some-rss-feed.com/rss_feed"
+EMAIL = "your@email.com"
+
+loop do
+
+  response = Net::HTTP.get_response(URI.parse(URL))
+  rss = RSS::Parser.parse(response.body)
+
+  results = rss.items.select do |item|
+    item.description =~ /regex to match description/i
+  end
+
+  entries = results.map { |r| [r.title, r.description, r.date, r.link] }
+
+  email_body = "Here's your list of things: "
+  entries.each do |entry|
+    email_body << "\n#{entry.join("\n")}\n"
+  end
+
+  Net::SMTP.start('localhost') do |smtp|
+    smtp.send_message(email_body, EMAIL, EMAIL)
+  end
+
+  sleep(60 * 60 * 24)
+
+end
