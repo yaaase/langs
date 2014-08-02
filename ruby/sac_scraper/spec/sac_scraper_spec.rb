afc87b954a73d994e6ec3e2f1b779c1aa64623f5
Mark Billie <mbillie1@gmail.com>
2014-08-02 13:35:08 -0600

    updating specs blah

--- ruby/sac_scraper/spec/sac_scraper_spec.rb
+++ ruby/sac_scraper/spec/sac_scraper_spec.rb
@@ -4,36 +4,36 @@ describe SteepAndCheapScraper do
   let(:s) { SteepAndCheapScraper.new }
 
   context "desired matches" do
-    it { s.match?("SKIS").should be_true }
+    it { expect(s.match?("SKIS")).to eq(true) }
 
-    it { s.match?("goggle").should be_true }
+    it { expect(s.match?("goggle")).to eq(true) }
 
-    it { s.match?("snow pant").should be_true }
+    it { expect(s.match?("snow pant")).to eq(true) }
 
-    it { s.match?("mountain hardwear").should be_true }
+    it { expect(s.match?("mountain hardwear")).to eq(true) }
 
-    it { s.match?("climbing skins").should be_true }
+    it { expect(s.match?("climbing skins")).to eq(true) }
   end
 
   context "remembering the title" do
     it "lets me know what the cool shit is" do
-      s.stub(:scrape_title).and_return("Black Diamond Verdict Ski")
-      s.should_receive(:mail).with("Black Diamond Verdict Ski http://www.steepandcheap.com/")
+      allow(s).to receive(:scrape_title).and_return("Black Diamond Verdict Ski")
+      expect(s).to receive(:mail).with("Black Diamond Verdict Ski http://www.steepandcheap.com/")
       s.scrape
     end
 
     it "does not spam me if the match is the same as before" do
-      s.stub(:scrape_title).and_return("Black Diamond Verdict Ski")
+      allow(s).to receive(:scrape_title).and_return("Black Diamond Verdict Ski")
       s.instance_variable_set(:@previous_title, "Black Diamond Verdict Ski http://www.steepandcheap.com/")
-      s.should_not_receive(:mail)
+      expect(s).to_not receive(:mail)
       s.scrape
     end
   end
 
   context "exclusions" do
     it "can exclude certain things, like gender specific items" do
-      s.stub(:scrape_title).and_return("Mountain Hardwear Women's Thing")
-      s.should_not_receive(:mail)
+      allow(s).to receive(:scrape_title).and_return("Mountain Hardwear Women's Thing")
+      expect(s).to_not receive(:mail)
       s.scrape
     end
   end
@@ -41,7 +41,7 @@ describe SteepAndCheapScraper do
   context "parse_title" do
     it "parses the title correctly" do
       site = "<html>\n<title>Title</title>\n<div>Some crap</div>\n\n</html>"
-      s.parse_title(site).should == "Title"
+      expect(s.parse_title(site)).to eq("Title")
     end
   end
 end
